1585051e81f656a720750502cc130f2a5a9124ed
Anugushashank <anugushashank@gmail.com>
2017-08-27 15:34:55 +0530

    Just doing things

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ IMAGE_TAG=${CIRCLE_SHA1}
 TASK_FAMILY=ecstest
 
 # Create a new task definition for this build
-aws ecs register-task-definition  --cli-input-json file://task-definition.json
+aws ecs register-task-definition  --cli-input-json file://task_definition.json
 
 # Update the service with the new task definition and desired count
-TASK_REVISION=`aws ecs describe-task-definition --task_definition ${TASK_FAMILY} | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+TASK_REVISION=`aws ecs describe-task-definition --task-definition ${TASK_FAMILY} | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 DESIRED_COUNT=`aws ecs describe-services --cluster ${CLUSTER_NAME} --services ${SERVICE_NAME} | egrep "desiredCount" | head -1 | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
 if [ ${DESIRED_COUNT} = "0" ]; then
     DESIRED_COUNT="1"
