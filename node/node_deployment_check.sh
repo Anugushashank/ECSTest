8b033b6a52b3122ed55b2a95431f54693a97b52d
Anugushashank <anugushashank@gmail.com>
2017-08-27 20:25:13 +0530

    Testing

--- node/node_deployment_check.sh
+++ node/node_deployment_check.sh
@@ -10,6 +10,7 @@ FOLDER1_COMMIT=$(git log -1 --format=format:%H --full-diff .)
 if [ $FOLDER1_COMMIT = $LATEST_COMMIT ];
     then
         echo "files in folder1 has changed"
+        chmod +x deploy.sh
         ./deploy.sh
 else
      echo "no folders of relevance has changed"
