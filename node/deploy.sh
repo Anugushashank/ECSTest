d7050a350249ae032c9c2bc216f6d18a770ed13d
Anugushashank <anugushashank@gmail.com>
2017-08-27 23:07:26 +0530

    Testing

--- node/deploy.sh
+++ node/deploy.sh
@@ -5,6 +5,11 @@ BUILD_NUMBER=${CIRCLE_BUILD_NUM}
 IMAGE_TAG=${CIRCLE_SHA1}
 TASK_FAMILY=ecstest
 
+docker tag ecrtest 878242495038.dkr.ecr.us-east-2.amazonaws.com/ecs_test:$CIRCLE_SHA1
+docker tag ecrtest 878242495038.dkr.ecr.us-east-2.amazonaws.com/ecs_test:latest
+docker push 878242495038.dkr.ecr.us-east-2.amazonaws.com/ecs_test:$CIRCLE_SHA
+docker push 878242495038.dkr.ecr.us-east-2.amazonaws.com/ecs_test:latest
+
 # Create a new task definition for this build
 aws ecs register-task-definition  --cli-input-json file://task_definition.json --region us-east-2
 
@@ -15,4 +20,4 @@ if [ ${DESIRED_COUNT} = "0" ]; then
     DESIRED_COUNT="1"
 fi
 
-aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count 2 --region us-east-2
+aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count 3 --region us-east-2
